7e550611260e737208d0f0cf343fe3e11268ebf0
Jaime Daza <jaimedavid250701@outlook.com>
2023-03-16 12:48:15 -0500

    feat: Set application card structure

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import {Col, Container, Row} from "react-bootstrap";
+import AppCard from "../components/AppCard";
+
+const sample = {
+  title: "Sample App",
+  body: "This is a sample app",
+  img: "https://picsum.photos/1000/500",
+  teams: [
+    {
+      website: "https://www.google.com",
+      name: "Team 1",
+    },
+    {
+      website: "https://www.google.com",
+      name: "Team 2",
+    },
+    {
+      website: "https://www.google.com",
+      name: "Team 3",
+    },
+  ],
+  count: 0,
+};
 
 function Overview() {
-  return <div>Overview</div>;
+  return (
+    <Container>
+      <Row>
+        <Col xs={6}>
+          <AppCard {...sample} />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Overview;
